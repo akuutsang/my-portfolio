c33678f9b7c6db2ecb407e0e67a255dc765d238a
akutsang <akuutsang@gmail.com>
2023-06-11 12:34:47 +0100

    add dynamic card utils.js

--- my-portfolio/resources/js/index.js
+++ my-portfolio/resources/js/index.js
@@ -2,5 +2,8 @@ import newIcon from './dark.js';
 
 import updateTime from './clock.js';
 
+import card from './utils.js';
+
 newIcon();
 updateTime();
+card();
---
+++ my-portfolio/resources/js/utils.js
@@ -0,0 +1,61 @@
+const data = [
+
+  {
+    name: 'project paper, rock, scissors',
+    description: 'A very popular game placed all across the world',
+    image: '/my-portfolio/resources/assets/projects/Paper-Rock-Scissors.JPG',
+    link: 'https://github.com/Phildeoner/Team-Project-PRS',
+  },
+
+  {
+    name: 'project calculator',
+    description: 'A replica of the famous iphone calculator',
+    image: '/my-portfolio/resources/assets/projects/Calculator.JPG',
+    link: 'https://github.com/akuutsang/Calculator',
+  },
+
+  {
+    name: 'project Etch-a-sketch',
+    description: 'A sketch pad',
+    image: '/my-portfolio/resources/assets/projects/Etch-A-Sketch.JPG',
+    link: 'https://github.com/akuutsang/Etch-A-Sketch',
+  },
+
+  {
+    name: 'Capstone project',
+    description: 'A collaborative work',
+    Image: '',
+    link: 'https://phildeoner.github.io/Capstone-Project/index.html',
+  },
+
+  {
+    name: ' Landing page',
+    description: 'A simple landing page',
+    image: '/my-portfolio/resources/assets/projects/Landing-Page.jpg',
+    link: 'https://github.com/akuutsang/landingPage-portfolio',
+  },
+
+];
+
+function card() {
+  data.forEach((element, i) => {
+    const projects = document.querySelector('.projects');
+    const box = document.createElement('div');
+    box.classList = 'boxes';
+    const boxItem = `
+                <img class= "img" src="${data[i].image}" alt="">
+                <h5>${data[i].name}</h5>
+                <h6>${data[i].description}</h6>
+                <div class="info">
+                <a class="click" href="${data[i].link}">click for more</a>
+                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                <p class="lang"> Html, Css, JavaScript</p>
+                </div>
+                `;
+
+    box.innerHTML += boxItem;
+    projects.appendChild(box);
+  });
+}
+
+export default card;
